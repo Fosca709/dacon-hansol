a342c97419fdc823772649df1e7b6dd55c2dec90
Fosca709 <141136674+Fosca709@users.noreply.github.com>
2025-02-21 10:49:56 +0900

    add few shot inference

--- src/dacon_hansol/inference.py
+++ src/dacon_hansol/inference.py
@@ -2,7 +2,7 @@ import polars as pl
 from transformers import GenerationMixin
 from transformers.tokenization_utils_base import PreTrainedTokenizerBase
 
-from .data import concat_fields
+from .data import concat_fields, load_data
 
 ZERO_SHOT_SYSTEM_PROMPT = """당신은 건설 안전 전문가입니다. 질문에 핵심 내용만 간략하게 답하세요. 서론, 배경 설명 또는 추가 설명 없이 바로 답변하세요."""
 ZERO_SHOT_QUESTION_PROMPT = "\n위와 같은 상황에서 재발 방지 대책 및 향후 조치 계획은 무엇인가요?"
@@ -40,6 +40,62 @@ def vllm_zero_shot(model, df: pl.DataFrame, max_new_tokens: int, **kwargs) -> pl
     conversations = [get_zero_shot_messages(text) for text in texts]
 
     sampling_params = SamplingParams(n=1, temperature=0, max_tokens=max_new_tokens, **kwargs)
+    outputs = model.chat(messages=conversations, sampling_params=sampling_params)
+    output_texts = [output.outputs[0].text for output in outputs]
+    return pl.Series(name="pred", values=output_texts)
+
+
+def get_default_few_shot_examples(num_examples: int) -> pl.DataFrame:
+    indices = [6162, 9414, 1228, 4681, 12920, 19761, 22766, 713]
+    assert num_examples <= len(indices)
+
+    df_train = load_data("train")
+    df_processed = concat_fields(df_train)
+    return df_processed[indices[:num_examples]]
+
+
+def get_messages_from_frame(
+    df_examples: pl.DataFrame,
+    system_prompt: str = ZERO_SHOT_SYSTEM_PROMPT,
+    question_prompt: str = ZERO_SHOT_QUESTION_PROMPT,
+) -> list[dict[str, str]]:
+    few_shot_messages = [{"role": "system", "content": system_prompt}]
+
+    for row in df_examples.iter_rows():
+        user_message = f"{row[0]}{question_prompt}"
+        few_shot_messages.append({"role": "user", "content": user_message})
+
+        assistant_message = row[1]
+        few_shot_messages.append({"role": "assistant", "content": assistant_message})
+
+    return few_shot_messages
+
+
+def get_few_shot_messages(
+    text: str, few_shot_messages: list[dict[str, str]], qeustion_prompt: str = ZERO_SHOT_QUESTION_PROMPT
+) -> list[dict[str, str]]:
+    messages = few_shot_messages.copy()
+    messages.append({"role": "user", "content": f"{text}{qeustion_prompt}"})
+    return messages
+
+
+def vllm_few_shot(
+    model,
+    df: pl.DataFrame,
+    few_shot_messages: list[dict[str, str]],
+    max_new_tokens: int,
+    temperature: float = 0,
+    **kwargs,
+) -> pl.Series:
+    from vllm import LLM, SamplingParams
+
+    assert isinstance(model, LLM)
+
+    df_processed = concat_fields(df)
+    texts = df_processed["text"].to_list()
+    conversations = [get_few_shot_messages(text, few_shot_messages) for text in texts]
+
+    sampling_params = SamplingParams(n=1, temperature=temperature, max_tokens=max_new_tokens, **kwargs)
     outputs = model.chat(messages=conversations, sampling_params=sampling_params)
     output_texts = [output.outputs[0].text for output in outputs]
     return pl.Series(name="pred", values=output_texts)
--- src/dacon_hansol/submission.py
+++ src/dacon_hansol/submission.py
@@ -6,7 +6,7 @@ from sentence_transformers import SentenceTransformer
 
 from . import SAVE_PATH
 from .data import load_data
-from .inference import vllm_zero_shot
+from .inference import get_default_few_shot_examples, get_messages_from_frame, vllm_few_shot, vllm_zero_shot
 from .model import MODEL_NAMES, load_ko_sbert_sts, load_vllm_chat_model
 
 
@@ -41,7 +41,8 @@ def sample_submission() -> pl.DataFrame:
 def zero_shot_submission(
     model_name: str = "varco", max_new_tokens: int = 64, sample_size: Optional[int] = None
 ) -> None:
-    model_name = MODEL_NAMES[model_name]
+    if model_name in MODEL_NAMES:
+        model_name = MODEL_NAMES[model_name]
     model = load_vllm_chat_model(model_name)
 
     df_test = load_data("test")
@@ -50,6 +51,31 @@ def zero_shot_submission(
 
     df_pred = vllm_zero_shot(model=model, df=df_test, max_new_tokens=max_new_tokens)
     df_pred.to_frame().write_json(SAVE_PATH / "pred.json")
+
+    embeddings = make_embeddings(df_pred=df_pred)
+    submission = make_submission(df=df_test, df_pred=df_pred, embeddings=embeddings)
+    submission.write_csv(SAVE_PATH / "submission.csv")
+
+
+def few_shot_submission(
+    model_name: str = "varco",
+    df_examples: Optional[pl.DataFrame] = None,
+    max_new_tokens: int = 64,
+    sample_size: Optional[int] = None,
+) -> None:
+    if df_examples is None:
+        df_examples = get_default_few_shot_examples(num_examples=4)
+    few_shot_messages = get_messages_from_frame(df_examples)
+
+    df_test = load_data("test")
+    if sample_size is not None:
+        df_test = df_test[:sample_size]
+
+    if model_name in MODEL_NAMES:
+        model_name = MODEL_NAMES[model_name]
+    model = load_vllm_chat_model(model_name)
+
+    df_pred = vllm_few_shot(model=model, df=df_test, few_shot_messages=few_shot_messages, max_new_tokens=max_new_tokens)
 
     embeddings = make_embeddings(df_pred=df_pred)
     submission = make_submission(df=df_test, df_pred=df_pred, embeddings=embeddings)
